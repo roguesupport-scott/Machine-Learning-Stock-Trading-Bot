a8fb7bd9041fa4a28bf4136ac8f64b3b2e6248e2
ygsamueljeong <46389189+ygsamueljeong@users.noreply.github.com>
2020-04-21 10:08:52 +0900

    Add files via upload

--- __main__.py
+++ __main__.py
@@ -1,88 +1,98 @@
 import alpaca_trade_api as tradeapi
+import pandas as pd
 from forecast_library import *
 from config import *
+import os
 import sys
 
 def buy():
     # Importing Twit Data from StockTwit Accounts
-    twit_data = importing_twitdata('topstockalert', 'StockAuthority')
+    twit_data = importing_twitdata('topstockalerts', 'StockAuthority')
     # Candidate Stocks Selected By Sentiment Analysis on Optimistic Twits
     candidate_stocks = twit_sentiment_analyzer(twit_data)
+    # Opening Text File To Store Target Pricing
+    f = open("target_price_list.txt", "a")
     # Analyzing Candidate Stocks (Not Currently Held in Position) with Historical Data
     for ticker in candidate_stocks.difference(my_positions):
-        stock_history = pulling_price_history(ticker)
-        current_price = float(stock_history['close'].head(1))
-        # Predicting Future Stock Price with Machine Learning Random Forest Regressor Model
-        target_price = float(random_forest_forecast(stock_history))
-        # Determining Num of Shares to Buy
-        if buying_power < 200:
-            qty_desired = int((buying_power/current_price) / len(candidate_stocks.difference(my_positions)))
-        else:
-            qty_desired = int(((target_price - current_price) / current_price) * buying_power)
-        # Submitting Affordable Buy Order on Market Price
-        if buying_power >= target_price*qty_desired > current_price*qty_desired:
-            alpaca_api.submit_order(
-                symbol=ticker,
-                qty=qty_desired,
-                side='buy',
-                type='market',
-                time_in_force='gtc'
-                )
+        try:
+            stock_history = pulling_price_history(ticker)
+            current_price = float(stock_history['close'].head(1))
+            # Predicting Future Stock Price with Machine Learning Random Forest Regressor Model
+            target_price = float(random_forest_forecast(stock_history))
+            # Determining Num of Shares to Buy
+            if buying_power < 350:
+                qty_desired = int((buying_power/current_price) / len(candidate_stocks.difference(my_positions)))-1
+            else:
+                qty_desired = int(((target_price - current_price) / current_price) * buying_power)
+            # Submitting Affordable Buy Order on Market Price
+            if buying_power >= (target_price * qty_desired) > (current_price * qty_desired) and qty_desired > 0:
+                alpaca_api.submit_order(
+                    symbol=ticker,
+                    qty=qty_desired,
+                    side='buy',
+                    type='market',
+                    time_in_force='gtc'
+                    )
+                # Save Target Price on a Separate Text File
+                f.write(ticker + " " + str(target_price) + "\n")
+        except ValueError:
+            pass
+    f.close()
     return print('Buy Orders Sent')
 
 
 def sell():
-    # Selling Positions that Does Not Have Pending Selling Orders
-    for position in my_positions.difference(my_orders):
-        # Importing Position Data
-        position_api = alpaca_api.get_position(position)
-        current_price = float(position_api.current_price)
-        price_change = float(position_api.unrealized_plpc)
-        qty_bought = float(position_api.qty)
-        # Adjusting Machine Learning Target Price with Updated Data
-        updated_stock_history = pulling_price_history(position)
-        updated_target_price = float(random_forest_forecast(updated_stock_history))
-        # Sell on -7.5% Loss or on Unexpected Profit (Beyond the Scope of Prediction)
-        if price_change <= -0.075 or price_change > ((updated_target_price-current_price)/current_price):
-            alpaca_api.submit_order(
-                symbol=position,
-                qty=qty_bought,
-                side='sell',
-                type='market',
-                time_in_force='gtc'
-            )
-        # Selling on Target Price or on Stop Price of -7.5%
-        else:
-            alpaca_api.submit_order(
-                symbol=position,
-                qty=qty_bought,
-                side='sell',
-                type='stop_limit',
-                limit_price=updated_target_price,
-                stop_price=round(current_price * (1-(-0.075-price_change)), 4),
-                time_in_force='gtc'
-            )
+    # Opening Saved File of Target Price List (Saved from Previous Trading Day)
+    f = open('target_price_list.txt', 'r+')
+    if os.stat('target_price_list.txt').st_size != 0:
+        f_content = f.readlines()
+        df = pd.DataFrame({'ticker': f_content}).ticker.str.split(expand=True)
+        # Selling Positions that Does Not Have Pending Selling Orders
+        for position in my_positions.difference(my_orders):
+            # Importing Position Data
+            position_api = alpaca_api.get_position(position)
+            price_change = float(position_api.unrealized_plpc)
+            current_price = float(position_api.current_price)
+            qty_bought = float(position_api.qty)
+            target_price = df.loc[df[0] == position, 1].astype(float).values[0]
+            target_percent_change = round(((target_price - current_price) / current_price), 2)
+            # Sell on 7.5% Loss, Inverted Targeted Percent Loss, or Targeted Profit R
+            if price_change <= (target_percent_change * -1) or price_change <= -0.075 or current_price >= target_price:
+                alpaca_api.submit_order(
+                    symbol=position,
+                    qty=qty_bought,
+                    side='sell',
+                    type='market',
+                    time_in_force='gtc'
+                )
+                # Omitting Sold Position Data from 'target_price_list.txt'
+                f.seek(0)
+                for line in f_content:
+                    if str(position) not in line:
+                        f.write(line)
+                f.truncate()
+    # Closing Text File
+    f.close()
     return print('Sell Orders Sent')
 
-
+# Main Function
 if __name__ == '__main__':
-    # Account Status Review
-    alpaca_api = tradeapi.REST(base_url=ALPACA_BASE_URL,
-                        key_id=ALPACA_API_KEY,
-                        secret_key=ALPACA_SECRET_KEY
-                        )
+    # Connecting to Alpaca Account
+    alpaca_api = tradeapi.REST(
+        base_url=ALPACA_BASE_URL,
+        key_id=ALPACA_API_KEY,
+        secret_key=ALPACA_SECRET_KEY
+        )
     # Liquidity
     buying_power = float(alpaca_api.get_account().buying_power)
-
     # Current Holding Positions
     open_positions = alpaca_api.list_positions()
     my_positions = set(map(lambda index: open_positions[index].symbol, range(len(open_positions))))
-
     # Open Orders
     open_orders = alpaca_api.list_orders(status='open')
     my_orders = set(map(lambda index: open_orders[index].symbol, range(len(open_orders))))
-
     # Executing Sell & Buy Orders
     sell()
     buy()
     sys.exit()
+
--- forecast_library.py
+++ forecast_library.py
@@ -12,7 +12,7 @@ def importing_twitdata(account1, account2):
     r = requests.get(('https://api.stocktwits.com/api/2/streams/user/{}.json').format(account1))
     r2 = requests.get(('https://api.stocktwits.com/api/2/streams/user/{}.json').format(account2))
     twit_data = (r.text + r2.text).split('body')
-    # Handpicking Twits that Literally Mention Tickers
+    # Handpicking Twits that Contain Ticker Names
     for line in twit_data:
         if '$' in line:
             twit_data[twit_data.index(line)] = line.split('created_at')[0].strip('\":\"')
@@ -25,31 +25,29 @@ def twit_sentiment_analyzer(twit_data):
     candidate_list = set()
     for twit_message in twit_data:
         # Sentiment Analysis: 0.4 or Higher Polarity --> Optimistic
-        if TextBlob(twit_message).sentiment.polarity > 0.4:
+        if TextBlob(twit_message).sentiment.polarity > 0.45:
             # Extracting Stock Ticker Name from Clustered Data
             for word in twit_message.split(' '):
-                if '$' in word and word[1:] not in candidate_list and len(word) in np.arange(2, 6):
+                if '$' in word and word[1:] not in candidate_list and '$' not in word[1:] and len(word) in np.arange(2, 6):
                     candidate_list.add(word[1:].upper())
     return candidate_list
 
-# Scrubbing Historical Stock Data: Readily Use for Machine Learning
+# Scrubbing Historical Stock Data: Preparing for Machine Learning Training
 def pulling_price_history(ticker):
     # Importing Stock Data and Relevant News Headlines
     stock_price_data = requests.get(('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={}&apikey='
                                      '{}demo&datatype=csv').format(ticker, ALPHAV_API_KEY)).text.splitlines()[1:]
     news_data = NEWS_API.get_everything(q=(ticker+' stock'), language='en')
-
     # Organizing Data into pd.DataFrame
     df = pd.DataFrame(pd.DataFrame(stock_price_data)[0].str.split(',', n=6, expand=True))
     df.columns = ['date', 'open', 'high', 'low', 'close', 'volume']
     df['close_price_%change'] = round((df['close'].astype(float) - df['open'].astype(float))
                                        /df['open'].astype(float) * 100, 2)
-
     # Adding "Press Release" and its "Sentiment Analysis" Data as DataFrame Columns
     headlines = {}
     news_content = list(news_data.values())
     if news_content[1] > 0:
-        # Handpicking "News Description" Data within Clustered List and Dict and Storing in "Headlines" Dictionary
+        # Handpicking "News Description" Data within Clustered List and Dict and Storing in "headlines" Dictionary
         for index in range(len(news_content[2])):
             # Identifying "Published Date" within Clustered List/Dict
             date = news_content[2][index]['publishedAt'].split('T')[0]
@@ -60,11 +58,11 @@ def pulling_price_history(ticker):
             df.loc[df['date'] == pr_date, 'related news'] = 1
             df.loc[df['date'] == pr_date, 'news_polarity'] = news_analysis.polarity
             df.loc[df['date'] == pr_date, 'news_subjectivity'] = news_analysis.subjectivity
-
     # Data Type Formatting
     df['date'] = pd.to_datetime(df['date'])
     df.set_index('date', inplace=True)
-    df[['open', 'high', 'low', 'close', 'volume']] = df[['open', 'high', 'low', 'close', 'volume']].apply(pd.to_numeric, errors='coerce')
+    df[['open', 'high', 'low', 'close', 'volume']] = df[['open', 'high', 'low', 'close', 'volume']].apply(
+        pd.to_numeric, errors='coerce')
     df.fillna(0, inplace=True)
     return df
 
@@ -76,7 +74,6 @@ def random_forest_forecast(data):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=101)
     forest = RandomForestRegressor(n_estimators=15)
     forest.fit(X_train, y_train)
-
     # Substitute of Technical Analysis: (Target Price = Maximum Forecast Price of the Next 20 Business Days * Accuracy Score)
     confidence_level = forest.score(X_test,y_test)
     prediction = round(forest.predict(np.array(X.head(20))).max() * confidence_level, 4)
