1596571a0cf9bec9eff29a25a8198e87819b1a71
ygsamueljeong <46389189+ygsamueljeong@users.noreply.github.com>
2021-05-08 13:29:26 -0400

    Delete check.py

--- check.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from forecast_library import *
-
-f = open('target_price_list.txt', 'r+')
-f_content = f.readlines()
-df = pd.DataFrame({'ticker': f_content}).ticker.str.split(expand=True)
-target_price = df.loc[df[0] == str("PLM"), 1].astype(float).values[0]
-
-print(target_price)
