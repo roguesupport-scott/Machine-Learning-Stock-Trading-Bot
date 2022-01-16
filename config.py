53961b1af6aad75eb0f2cd301ece14ac7084a5e3
ygsamueljeong <46389189+ygsamueljeong@users.noreply.github.com>
2022-01-16 14:49:31 -0500

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from newsapi import NewsApiClient
 #Storing Sensitive Data
 
 # Machine Learning Data API KEYS: ALPHA Vantage & NewsAPI
-ALPHAV_API_KEY = 'F77WsSTERDOLMHML3'
-NEWS_API = NewsApiClient(api_key='8eb47bc073c34a779816f1facfa0ccf0')
+ALPHAV_API_KEY=
+NEWS_API = NewsApiClient(api_key=)
 
 # Trading Execution Keys: Alpaca
 ALPACA_BASE_URL = 'https://paper-api.alpaca.markets'
-ALPACA_API_KEY = 'PK322IPFWV9K8EE7ZXP0'
-ALPACA_SECRET_KEY = '2IEIeI3SIeJtbai4OKCirGZGxi7ztPbxHq2Cn4Cs'
+ALPACA_API_KEY = 
+ALPACA_SECRET_KEY = 
 
